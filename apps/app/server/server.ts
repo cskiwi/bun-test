0e62d283eeec209530b55938090325ebdfbc5701
Glenn Latomme <glenn.latomme@arcelormittal.com>
2024-05-28 14:25:35 +0200

    chore: some update

--- apps/app/server/server.ts
+++ apps/app/server/server.ts
@@ -1,8 +1,8 @@
 import { APP_BASE_HREF } from '@angular/common';
 import { CommonEngine } from '@angular/ssr';
 import express from 'express';
-import { fileURLToPath } from 'node:url';
-import { dirname, join, resolve } from 'node:path';
+import { fileURLToPath } from 'url';
+import { dirname, join, resolve } from 'path';
 import bootstrap from '../src/main.server';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from '@app/backend-root';
