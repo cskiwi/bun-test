16ad4d5d9c3d3de9f5538ca839cbc5270a8fb0cb
Glenn Latomme <glenn.latomme@arcelormittal.com>
2024-05-28 11:01:43 +0200

    adding http request for testing

--- libs/frontend/root/src/app.component.ts
+++ libs/frontend/root/src/app.component.ts
@@ -1,18 +1,17 @@
-import { HttpClient } from '@angular/common/http';
+import { JsonPipe } from '@angular/common';
+import { HttpClient, provideHttpClient } from '@angular/common/http';
 import { Component, inject } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import {toSignal} from '@angular/core/rxjs-interop';
-
+import { derivedAsync } from 'ngxtension/derived-async';
 
 @Component({
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, JsonPipe],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  constructor() {
-    const data = fetch(`localhost:4200/api/backend-test`).then((res) => res.json());
-  }
+  private readonly httpClient = inject(HttpClient);
+  movie = derivedAsync(() => this.httpClient.get(`/api/backend-test`));
 }
